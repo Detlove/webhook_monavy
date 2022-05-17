412d14a94b634806e8303ee599cd8ed9aa548f8c
David Eusebio <davidetlove@gmail.com>
2022-05-16 21:39:36 -0500

    more

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -11,7 +11,8 @@ const templates = {
   },
   information: () => {
     return {
-      message: 'Claro que sí, los guantes estan fabricados de 60% Algodón, 33% Poliéster y 7% Elastano de grado médico, perfectos para aliviar el dolor causado por las artritis, son lavables y su precio es de $59,900 pesos colombianos\nSi desea pedirlos, necesitamos la siguiente información por favor: Nombre y Apellidos, Teléfono, Departamento, Ciudad, Dirección y Talla'
+      message: 'Claro que sí, los guantes estan fabricados de 60% Algodón, 33% Poliéster y 7% Elastano de grado médico.\n\nPerfectos para aliviar el dolor causado por las artritis, son lavables y su precio es de $59,900 pesos colombianos\n\nSi tiene alguna pregunta no dude en compartirla con nosotros',
+      priority: 'urgent'
     }
   }
 }
