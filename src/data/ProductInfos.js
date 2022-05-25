a0e573797b4641e0baad4864a0295d1d18f47fb9
David Eusebio <davidetlove@gmail.com>
2022-05-25 15:06:41 -0500

    more

--- src/data/ProductInfos.js
+++ src/data/ProductInfos.js
@@ -7,7 +7,7 @@ module.exports = {
     }
   },
   'Guantes de Compresión con Infusión De Cobre para Dolor de manos, Artritis y Túnel carpiano': {
-    message: 'Los guantes están fabricados de *85%* fibra de cobre terapéutico, y *7%* Elastano de grado médico, son lavables, perfectos para aliviar el dolor causado por las artritis o túnel carpiano, el precio es de *$79,900* pesos colombianos',
+    message: 'Los guantes están fabricados de *85%* fibra de cobre terapéutico, y *15%* elastano de grado médico, son lavables, perfectos para aliviar el dolor causado por las artritis o túnel carpiano, el precio es de *$79,900* pesos colombianos',
     media: {
       file: '628e89f3d130d69af26c8751',
       format: 'native'
