225c7e6b994cda4cd0eb91805833b36476d52428
David Eusebio <davidetlove@gmail.com>
2022-06-06 21:17:55 -0500

    change product info name

--- src/data/ProductInfos.js
+++ src/data/ProductInfos.js
@@ -1,12 +1,12 @@
 module.exports = {
-  'Guantes de Compresión Suave para Artritis, Artrosis, Tendinitis y Túnel Carpiano': {
+  'Guantes de Compresión Suave para Artritis': {
     message: 'Los guantes están fabricados de *60%* Algodón, *33%* Poliéster y *7%* Elastano de grado médico, son lavables, perfectos para aliviar el dolor causado por las artritis o túnel carpiano, el precio es de *$69,900* pesos colombianos',
     media: {
       file: '628312c211023a7b1e0c2735',
       format: 'native'
     }
   },
-  'Guantes de Compresión con Infusión De Cobre para Dolor de manos, Artritis y Túnel carpiano': {
+  'Guantes de Compresión con Infusión De Cobre': {
     message: 'Los guantes están fabricados de *85%* fibra de cobre terapéutico, y *15%* elastano de grado médico, son lavables, perfectos para aliviar el dolor causado por las artritis o túnel carpiano, el precio es de *$79,900* pesos colombianos',
     media: {
       file: '628e89f3d130d69af26c8751',
